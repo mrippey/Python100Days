611afbcf2fe3d9e685bc18ddc7f1296e75bf7633
Mike R <30969281+msec1203@users.noreply.github.com>
2021-05-08 07:17:23 +0900

    Update data_structures_mod.py

--- days_seven_nine_100days/data_structures_mod.py
+++ days_seven_nine_100days/data_structures_mod.py
@@ -1,1 +1,59 @@
+us_state_abbrev = {'Alabama': 'AL', 'Alaska': 'AK', 'Arizona': 'AZ',
+                   'Arkansas': 'AR', 'California': 'CA', 'Colorado': 'CO',
+                   'Connecticut': 'CT', 'Delaware': 'DE', 'Florida': 'FL',
+                   'Georgia': 'GA', 'Hawaii': 'HI', 'Idaho': 'ID',
+                   'Illinois': 'IL', 'Indiana': 'IN', 'Iowa': 'IA',
+                   'Kansas': 'KS', 'Kentucky': 'KY', 'Louisiana': 'LA',
+                   'Maine': 'ME', 'Maryland': 'MD', 'Massachusetts': 'MA',
+                   'Michigan': 'MI', 'Minnesota': 'MN', 'Mississippi': 'MS',
+                   'Missouri': 'MO', 'Montana': 'MT', 'Nebraska': 'NE',
+                   'Nevada': 'NV', 'New Hampshire': 'NH', 'New Jersey': 'NJ',
+                   'New Mexico': 'NM', 'New York': 'NY',
+                   'North Carolina': 'NC', 'North Dakota': 'ND',
+                   'Ohio': 'OH', 'Oklahoma': 'OK', 'Oregon': 'OR',
+                   'Pennsylvania': 'PA', 'Rhode Island': 'RI',
+                   'South Carolina': 'SC', 'South Dakota': 'SD',
+                   'Tennessee': 'TN', 'Texas': 'TX', 'Utah': 'UT',
+                   'Vermont': 'VT', 'Virginia': 'VA', 'Washington': 'WA',
+                   'West Virginia': 'WV', 'Wisconsin': 'WI', 'Wyoming': 'WY'}
 
+states = ['Oklahoma', 'Kansas', 'North Carolina', 'Georgia', 'Oregon',
+          'Mississippi', 'Minnesota', 'Colorado', 'Alabama',
+          'Massachusetts', 'Arizona', 'Connecticut', 'Montana',
+          'West Virginia', 'Nebraska', 'New York', 'Nevada', 'Idaho',
+          'New Jersey', 'Missouri', 'South Carolina', 'Pennsylvania',
+          'Rhode Island', 'New Mexico', 'Alaska', 'New Hampshire',
+          'Tennessee', 'Washington', 'Indiana', 'Hawaii', 'Kentucky',
+          'Virginia', 'Ohio', 'Wisconsin', 'Maryland', 'Florida',
+          'Utah', 'Maine', 'California', 'Vermont', 'Arkansas', 'Wyoming',
+          'Louisiana', 'North Dakota', 'South Dakota', 'Texas',
+          'Illinois', 'Iowa', 'Michigan', 'Delaware']
+
+
+# Task 1: Print out the 10th item in each
+def get_tenth_item_each(lst=states, dct=us_state_abbrev):
+    tenth_item_lst = [state for count, state in enumerate(lst, start=1) if count == 10]
+    tenth_item_dict = list(st for st in dct.items())[10]
+    return f'10th Item from List: {tenth_item_lst}, 10th Item from Dict: {tenth_item_dict}'
+
+#print(get_tenth_item_each())
+
+
+# Task 2: Print out the 45th key in the dictionary
+def get_forty_fifth_item(dct=us_state_abbrev):
+    return f'45th dict key: {list(dic1 for dic1 in dct.keys())[45]}'
+
+#print(get_forty_fifth_item())
+
+
+# Task 3: Print out the 27th value in the dictionary
+def get_twenty_seventh_item(dct=us_state_abbrev):
+    return f'27th dict value: {list(x for x in dct.values())[27]}'
+
+#print(get_twenty_seventh_item())
+
+def main():
+    print(get_tenth_item_each(), "\n"+get_forty_fifth_item(), "\n"+get_twenty_seventh_item())
+
+if __name__ == "__main__":
+    main()
